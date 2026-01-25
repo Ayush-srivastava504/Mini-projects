8c5c641a2e912f9fcbc060524958a741d3a65edc
Ayush-srivastava504 <73691270+Ayush-srivastava504@users.noreply.github.com>
2026-01-26 04:47:58 +0530

    Day5_done

--- python_utility/day5-emoji_enhancer_for_message.py
+++ python_utility/day5-emoji_enhancer_for_message.py
@@ -1,3 +1,32 @@
 #THis is day 5
 # Emoji Enhancer for Messages
-                                                                                                                                                                                                                                 
+   
+# get a dictionary
+emoji_map_dict = {
+    "happy": "😊",
+    "sad": "😞",
+    "wink": "😉",
+    "laugh": "😄",
+    "tongue": "😛",
+    "love": "❤️"
+}
+
+# get user message
+message = input("Enter your message: ")
+
+updated_message = []
+
+#process each word
+for word in message.split():
+    cleaned = word.lower().strip(",!.?")
+    emoji = emoji_map_dict.get(cleaned, "")
+    if emoji:
+        updated_message.append(f"{word}, {emoji}")
+    else:
+        updated_message.append(word)
+
+updated_word = "".join(updated_message)
+        
+print("\n Enhanced_message \n")
+print(updated_word)
+                                                                                                                                                                                                                              
